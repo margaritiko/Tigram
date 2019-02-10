20aeca7c13eaaad36de7948adade5642eb62dd31
Margarita <margaritiko@gmail.com>
2019-02-10 23:09:14 +0300

    New Mark comments and some changes in .gitignore

--- Tigram/AppDelegate.swift
+++ Tigram/AppDelegate.swift
@@ -5,6 +5,9 @@
 //  Created by Маргарита Коннова on 10/02/2019.
 //  Copyright © 2019 Margarita Konnova. All rights reserved.
 //
+
+// MARK: Logs available only in Debug mode
+// Change to Release in Product -> Scheme -> Edit scheme... -> Build Configuration and logs will be disabled
 #if !DEBUG
 func print(_ string: String) {}
 #endif
@@ -16,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    // MARK: Application lifecycle
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("Application moved from Not running to Foreground (Inactive): [\(#function)]")
