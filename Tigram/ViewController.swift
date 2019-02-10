2ae56f6333f7efc937f88dcbf517173036123770
Margarita <margaritiko@gmail.com>
2019-02-10 16:19:20 +0300

    Initial Commit

---
+++ Tigram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tigram
+//
+//  Created by Маргарита Коннова on 10/02/2019.
+//  Copyright © 2019 Margarita Konnova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
