94167f6c702b6e2f599881b27c3d84b71a407ae8
Margarita <margaritiko@gmail.com>
2019-02-17 23:53:18 +0300

    Finally all logs added

--- Tigram/ViewController.swift
+++ Tigram/ViewController.swift
@@ -7,65 +7,106 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Outlets
-    
-    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var editButton: UIButton?
     @IBOutlet weak var userPhoto: UIImageView!
     @IBOutlet weak var cameraButton: UIButton!
     
-    @IBAction func cameraButtonClicked(_ sender: Any) {
+    let imagePickerController = UIImagePickerController()
+    
+    // The alert allows user to choose between photo library and camera
+    let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+    
+    // MARK: ViewController life cycle
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
         
+        // Условие не выполнится:
+        // В данный момент outlets еще не установлены и имеют значение nil
+        // Обращаться к ним нельзя
+        if let editButtonFrame = editButton?.frame {
+            print("editButton.frame in \(#function): \(editButtonFrame)")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePickerController.delegate = self
+        settingUpAnAlert()
         
         // Edit button's design details
-        editButton.layer.cornerRadius = 10
-        editButton.layer.borderColor = UIColor.black.cgColor
-        editButton.layer.borderWidth = 1
-        editButton.contentEdgeInsets = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("ViewController Lifecycle: [\(#function)]")
+        editButton!.layer.cornerRadius = 10
+        editButton!.layer.borderColor = UIColor.black.cgColor
+        editButton!.layer.borderWidth = 1
+        editButton!.contentEdgeInsets = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        
+        // Обрабатывать значения геометрии в данном методе нельзя, так как они еще некорректны
+        // Все constraints не установлены, поэтому текущее значение frame считается без их учета и неверно
+        print("editButton.frame in \(#function): \(editButton!.frame)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("ViewController Lifecycle: [\(#function)]")
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        print("ViewController Lifecycle: [\(#function)]")
+        // На данном этапе все constraints установлены
+        // Значение frame отличается от предыдущего, так как оно уже корректно и рассчитано с учетом всех constraints
+        print("editButton.frame in \(#function): \(editButton!.frame)")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        print("ViewController Lifecycle: [\(#function)]")
         
-        // The right place to work with geometry
+        // The right place for work with geometry
         userPhoto.layer.cornerRadius = cameraButton.bounds.height / 2
         cameraButton.layer.cornerRadius = cameraButton.bounds.height / 2
         let paddings = cameraButton.bounds.height * 0.22
         cameraButton.imageEdgeInsets = UIEdgeInsets(top: paddings, left: paddings, bottom: paddings, right: paddings)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("ViewController Lifecycle: [\(#function)]")
+    // MARK: Actions
+    @IBAction func cameraButtonClicked(_ sender: Any) {
+        self.present(alert, animated: true, completion: nil)
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("ViewController Lifecycle: [\(#function)]")
+    // MARK: AlertController
+    func settingUpAnAlert() {
+        // First action in list
+        let choosePhotoAction = UIAlertAction(title: "Установить из галлереи", style: .default) { (action:UIAlertAction) in
+            self.imagePickerController.sourceType = .photoLibrary
+            self.present(self.imagePickerController, animated: true, completion: nil)
+        }
+        // Second action in list
+        let takePhotoAction = UIAlertAction(title: "Сделать фото", style: .default) { (action:UIAlertAction) in
+            // When launched, the device will be asked for permission to use the camera
+            
+            self.imagePickerController.sourceType = .camera
+            self.present(self.imagePickerController, animated: true, completion: nil)
+        }
+        alert.addAction(choosePhotoAction)
+        alert.addAction(takePhotoAction)
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Закрыть", comment: "Default action"), style: .cancel))
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            print("Not an image")
+            return
+        }
+        // Set new user's photo
+        userPhoto.image = selectedImage
+        // Dismiss the picker
+        dismiss(animated: true, completion: nil)
     }
 
 }
-
